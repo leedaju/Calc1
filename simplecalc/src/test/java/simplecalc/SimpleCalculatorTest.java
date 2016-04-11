f4e4f7a167de3a2f3649500f08ecb77f62b03833
multimedia <multimedia@309-30>
2016-04-11 17:15:57 +0900

    곱셈 기능과 테스트 기능 추가
    기본기능

--- simplecalc/src/test/java/simplecalc/SimpleCalculatorTest.java
+++ simplecalc/src/test/java/simplecalc/SimpleCalculatorTest.java
@@ -18,5 +18,11 @@ public class SimpleCalculatorTest {
 		calc.sub(10, 20);
 		assertEquals(-10,calc.getResult());
 	}
+	@Test
+	public void testMulti() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.multi(10, 20);
+		assertEquals(200,calc.getResult());
+	}
 
 }
