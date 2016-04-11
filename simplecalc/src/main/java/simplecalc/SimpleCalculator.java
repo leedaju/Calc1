7699eae627b856afe0d7b35b79fb267238058258
multimedia <multimedia@309-30>
2016-04-11 17:37:44 +0900

    나눗셈 오류 수정

--- simplecalc/src/main/java/simplecalc/SimpleCalculator.java
+++ simplecalc/src/main/java/simplecalc/SimpleCalculator.java
@@ -13,8 +13,10 @@ public class SimpleCalculator {
 		result = x * y;
 	}
 	public void div(int x, int y){
-		result = x / y;
-
+		if(y!=0)
+			result = x / y;
+		else
+			System.out.println("분모가 0입니다.");
 	}
 	public int getResult(){
 		return result;
--- simplecalc/src/test/java/simplecalc/SimpleCalculatorTest.java
+++ simplecalc/src/test/java/simplecalc/SimpleCalculatorTest.java
@@ -19,18 +19,18 @@ public class SimpleCalculatorTest {
 		assertEquals(-10,calc.getResult());
 	}
 	@Test
-<<<<<<< HEAD
+
 	public void testMulti() {
 		SimpleCalculator calc = new SimpleCalculator();
 		calc.multi(10, 20);
 		assertEquals(200,calc.getResult());
 	}
 
-=======
+
 	public void testDiv() {
 		SimpleCalculator calc = new SimpleCalculator();
 		calc.div(100, 20);
 		assertEquals(5,calc.getResult());
 	}
->>>>>>> refs/heads/divfunc
+
 }
